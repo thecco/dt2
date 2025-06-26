0b7ca33da60a4ca8507d3ee36d74dd1eff23b95f
thecco <thecco@naver.com>
2025-06-26 12:07:02 +0900

    쉐이더 다고침

--- src/dt/components/materials/GlowMaterial.js
+++ src/dt/components/materials/GlowMaterial.js
@@ -1,39 +1,47 @@
 import * as THREE from 'three';
 
 export function GlowMaterial({ glowColor = new THREE.Color(0x00ffff), c = 1.0, p = 2.7 }) {
-	return new THREE.ShaderMaterial({
-		uniforms: {
-			glowColor: { value: glowColor },
-			c: { value: c },
-			p: { value: p }
-		},
-		vertexShader: `
-      		uniform float c;
-      		uniform float p;
-      		varying float intensity;
+    const material = new THREE.MeshStandardMaterial({
+        transparent: true,
+        blending: THREE.AdditiveBlending,
+        depthWrite: false,
+    });
 
-      		void main() {
-      			vec3 vNormal = normalize(normalMatrix * normal);
-      			float dotView = abs(dot(vNormal, vec3(0.0, 0.0, 1.0)));
-      			float clamped = clamp(c - dotView, 0.0, 1.0);
-      			intensity = pow(clamped, p);
-      			gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-      		}
-		`,
-		fragmentShader: `
-			uniform vec3 glowColor;
-			varying float intensity;
+    material.onBeforeCompile = (shader) => {
+        shader.uniforms.glowColor = { value: glowColor };
+        shader.uniforms.c = { value: c };
+        shader.uniforms.p = { value: p };
 
-			void main() {
-				gl_FragColor = vec4(glowColor, intensity);
-			}
-   		`,
-		side: THREE.FrontSide,
-		transparent: true,
-		blending: THREE.AdditiveBlending,
-		depthWrite: false,
-				polygonOffset: true,
-		polygonOffsetFactor: -1,
-		polygonOffsetUnits: -4
-	});
+        shader.vertexShader = `
+            varying float intensity;
+            uniform float c;
+            uniform float p;
+        ` + shader.vertexShader;
+
+        shader.vertexShader = shader.vertexShader.replace(
+            '#include <fog_vertex>',
+            `
+            #include <fog_vertex>
+            vec3 viewNormal = normalize( normalMatrix * objectNormal );
+            float dotView = abs(dot(viewNormal, vec3(0.0, 0.0, 1.0)));
+            float clamped = clamp( c - dotView, 0.0, 1.0 );
+            intensity = pow( clamped, p );
+            `
+        );
+        
+        shader.fragmentShader = `
+            varying float intensity;
+            uniform vec3 glowColor;
+        ` + shader.fragmentShader;
+
+        shader.fragmentShader = shader.fragmentShader.replace(
+            '#include <dithering_fragment>',
+            `
+            #include <dithering_fragment>
+            gl_FragColor = vec4( glowColor, intensity );
+            `
+        );
+    };
+
+    return material;
 }
